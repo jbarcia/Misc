d4780004f9e676760dff042200be9cb844061451
jbarcia <jbarcia99@yahoo.com>
2015-08-17 23:29:28 -0400

    msf_start

--- msf_start.sh
+++ msf_start.sh
@@ -3,4 +3,4 @@ mkdir -p ~/tmp
 mkdir -p ~/tmp/msflogs
 service postgresql start
 service metasploit start
-msfconsole | tee ~/tmp/msflogs/`date +%m-%d-%Y_%H-%M-%S`.log
+msfconsole | tee /root/msf.`date +%m-%d-%Y_%H-%M-%S`.log
